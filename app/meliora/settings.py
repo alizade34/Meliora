0b121edbb383d1f1f1bf71f24ac3379cf363c9f5
Emil Alizada <emilalizade2001@gmail.com>
2023-05-03 16:45:52 +0400

    CSRF TOKEN HAS BEEN DELETED

--- app/meliora/settings.py
+++ app/meliora/settings.py
@@ -147,11 +147,10 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 
-CSRF_TRUSTED_ORIGINS=['https://admin.trustcenterholding.com', 'https://localhost', 'http://localhost']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CSRF_COOKIE_SECURE = True
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
