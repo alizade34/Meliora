613f7c86814f74a02779f101c59a948106d68b14
Emil Alizada <emilalizade2001@gmail.com>
2023-05-03 14:39:38 +0400

    first commit

---
+++ app/meliora/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name="api-docs"
+    ),
+    path('', include('accounts.urls')),
+    path('api/accounts/', include('accounts.api.urls')),
+]
+
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
