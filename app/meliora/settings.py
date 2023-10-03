3c5d4d22ae234345669428a8b7aa22827efeda84
Emil Alizada <emilalizade2001@gmail.com>
2023-10-03 18:13:01 +0400

    settings.py updated

--- app/meliora/settings.py
+++ app/meliora/settings.py
@@ -181,7 +181,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = 'emilalizade2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'dabonwbpdneujudh'
+EMAIL_HOST_PASSWORD = '#'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
