fa7c8629f5689370fc3d6aa3f3ac674592f2c88f
Emil Alizada <emilalizade2001@gmail.com>
2023-12-23 14:07:06 +0100

    ReUPLOADing

--- app/meliora/settings.py
+++ app/meliora/settings.py
@@ -110,7 +110,16 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 # REST_FRAMEWORK = {
 #     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 # }
